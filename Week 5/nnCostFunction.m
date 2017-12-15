9d862169de9ab9dcd311984f9dd3657eebdde3a1
gautam-sundar <v1z2e3@u.northwestern.edu>
2017-12-15 16:52:26 +0530

    Added comments to nnCostFunction.m

--- Week 5/nnCostFunction.m
+++ Week 5/nnCostFunction.m
@@ -17,10 +17,10 @@ function [J grad] = nnCostFunction(nn_params, ...
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
 Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+                 hidden_layer_size, (input_layer_size + 1)); % Theta1 is a 25 x 401 matrix
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+                 num_labels, (hidden_layer_size + 1)); % Theta2 is a 10 x 26 matrix
 
 % Setup some useful variables
 m = size(X, 1);
